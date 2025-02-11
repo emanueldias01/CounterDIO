412281bbff101a7252b6b37c320ea6fc2abdb743
emanueldias01 <emanueldias01dev@gmail.com>
2025-02-11 17:33:42 -0300

    implementando logica do desafio

--- untitled/src/Contador.java
+++ untitled/src/Contador.java
@@ -1,3 +1,5 @@
+import edu.dio.challenge.exceptions.ParametrosInvalidosException;
+
 import java.util.Scanner;
 
 public class Contador {
@@ -12,15 +14,23 @@ public class Contador {
             //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
 
-        }catch (Exception exception) {
+        }catch (ParametrosInvalidosException exception) {
             //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println(exception.getLancamento());
+            System.out.println(exception.getMensagem());
         }
 
     }
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }
 
         int contagem = parametroDois - parametroUm;
         //realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem ; i++) {
+            System.out.println("Imprimindo " + i);
+        }
     }
 }
